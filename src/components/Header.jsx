395601663c248ce494246797e86c3f643b3592f0
Vitalii Batrakov <vfingerz@yandex.ru>
2023-01-14 18:40:45 +0500

    add components Header, Navbar, Profile

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://w7.pngwing.com/pngs/637/10/png-transparent-logo-leaf-green-leaf-leaf-logo-business.png" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <img
+        className="image-content"
+        src="https://kartinkin.net/pics/uploads/posts/2022-08/1660508634_7-kartinkin-net-p-oboi-visokogo-kachestva-krasivo-7.jpg"
+      ></img>
+      <div>ava + descr</div>
+      <div>
+        My post
+        <div>New post</div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
