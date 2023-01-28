1816c20fc708783ada2acf2ee88bbc254935df20
Vitalii Batrakov <vfingerz@yandex.ru>
2023-01-28 19:32:00 +0500

    add components (Music, News, Settings,), add Routes and active style

---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <img src="https://w7.pngwing.com/pngs/637/10/png-transparent-logo-leaf-green-leaf-leaf-logo-business.png" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,9 @@
+import styles from "./Header.module.css";
+
+const Music = () => {
+  return (
+    Music
+  );
+};
+
+export default Music;
